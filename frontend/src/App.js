43e4f483799bb50a92261bf048e27b5a1b851152
Roman Montoya <59694362+Roman4u@users.noreply.github.com>
2022-04-28 16:47:48 +0900

    add: routes to the header and notelist component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,24 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
-import { useEffect, useState } from 'react';
+//import { useEffect, useState } from 'react';
 import './App.css';
 import Header from './components/Header';
 import NotesListPage from './pages/NotesListPage';
 
 
+
 function App() {
    
   return (
-    <div className="App">
-      <Header />
-      <NotesListPage  />
-    </div>
+    <Router>
+      <div className="App">
+      <Routes>
+          {/* <Header /> */ }
+          <Route path="/" exact element={<Header />}  />
+          <Route path="/noteslistpage" exact element={<NotesListPage />} />
+      </Routes>
+      </div>
+    </Router>
   );
 }
 
